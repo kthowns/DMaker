a36cf8a0930a81ba4c4182db98f10ba5b83c0c47
KTH <pni2396@gmail.com>
2024-09-23 23:19:33 +0900

    fix: Refactored parameters changed all request parameters to final

--- src/main/java/com/example/dmaker01/controller/DMakerController.java
+++ src/main/java/com/example/dmaker01/controller/DMakerController.java
@@ -27,7 +27,7 @@ public class DMakerController {
 
     @PostMapping("/create-developer")
     public CreateDeveloper.Response createDeveloper(
-            @Valid @RequestBody CreateDeveloper.Request request
+            @Valid @RequestBody final CreateDeveloper.Request request
     ) {
         log.info("request : {}", request);
 
@@ -36,7 +36,7 @@ public class DMakerController {
 
     @GetMapping("/developer/{memberId}")
     public DeveloperDetailDto getDeveloperDetail(
-            @PathVariable String memberId
+            @PathVariable final String memberId
     ) {
         log.info("GET /developer/" + memberId + " HTTP/1.1");
 
@@ -46,8 +46,8 @@ public class DMakerController {
 
     @PutMapping("/developer/{memberId}")
     public DeveloperDetailDto editDeveloper(
-            @PathVariable String memberId,
-            @Valid @RequestBody EditDeveloper.Request request
+            @PathVariable final String memberId,
+            @Valid @RequestBody final EditDeveloper.Request request
     ) {
         log.info("PUT /developer/" + memberId + " HTTP/1.1");
 
@@ -56,7 +56,7 @@ public class DMakerController {
 
     @DeleteMapping("/developer/{memberId}")
     public RetiredDeveloperDto deleteDeveloper(
-            @PathVariable String memberId
+            @PathVariable final String memberId
     ) {
         log.info("DELETE /developer/" + memberId + " HTTP/1.1");
 
