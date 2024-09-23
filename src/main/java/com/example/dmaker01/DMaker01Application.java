bfe3a7b00cc0bb103ba720e8b1fb2261b8b3e43a
KTH <pni2396@gmail.com>
2024-09-23 16:09:42 +0900

    Auditing Refactoring
    Delegated auditing to config Class

--- src/main/java/com/example/dmaker01/DMaker01Application.java
+++ src/main/java/com/example/dmaker01/DMaker01Application.java
@@ -3,7 +3,6 @@ package com.example.dmaker01;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
 @Slf4j
