888970d7c54224a540de265f9dc3e19692bb3feb
KTH <pni2396@gmail.com>
2024-09-24 22:47:21 +0900

    fix: Refactored validation in fucking awesome ways reduced so lots of lines of code. just check the codes out closely.

--- src/main/java/com/example/dmaker01/type/DeveloperLevel.java
+++ src/main/java/com/example/dmaker01/type/DeveloperLevel.java
@@ -1,16 +1,30 @@
 package com.example.dmaker01.type;
 
+import com.example.dmaker01.constant.DMakerConstant;
+import com.example.dmaker01.exception.DMakerErrorCode;
+import com.example.dmaker01.exception.DMakerException;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+
+import java.util.function.Function;
+
+import static com.example.dmaker01.constant.DMakerConstant.MAX_JUNIOR_EXPERIENCE_YEAR;
+import static com.example.dmaker01.constant.DMakerConstant.MIN_SENIOR_EXPERIENCE_YEAR;
 
 @AllArgsConstructor
 @Getter
 public enum DeveloperLevel {
-    NEW("신입 개발자"),
-    JUNIOR("주니어 개발자"),
-    JUNGNIOR("중니어 개발자"),
-    SENIOR("시니어 개발자")
-    ;
+    NEW("신입 개발자", years -> years == 0),
+    JUNIOR("주니어 개발자", years -> years <= MAX_JUNIOR_EXPERIENCE_YEAR),
+    JUNGNIOR("중니어 개발자", years -> years > MAX_JUNIOR_EXPERIENCE_YEAR
+            && years < MIN_SENIOR_EXPERIENCE_YEAR),
+    SENIOR("시니어 개발자", years -> years >= MIN_SENIOR_EXPERIENCE_YEAR);
 
     private final String description;
+    private final Function<Integer, Boolean> validateFunction;
+
+    public void validateExperienceYears(Integer years){
+        if(!validateFunction.apply(years))
+            throw new DMakerException(DMakerErrorCode.LEVEL_EXPERIENCE_YEARS_NOT_MATCHED);
+    }
 }
